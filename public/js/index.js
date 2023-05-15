556be5eeb89b9f4be22f13eefeaa84f10f268a66
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2023-05-15 10:24:04 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,29 @@
 const texts = document.querySelector('.texts');
 
+//Importing the Speech Interface, the regular and webkit version 
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+//Setting a variable to "recognition"
 
 const recognition = new window.SpeechRecognition();
 recognition.interimResults= true; 
 
 let p = document.createElement('p');
 
+//Adds an event listener to attach to a event with anew fucntion
 recognition.addEventListener('results',(e) => {
-  console.log(e);
+  
+  //putting text together using an array, use the join method to join them together. Use the map method 
+  
+  const text = Array.from(e.results)
+    .map(result => result[0])  
+    .map(result => result.transcript)
+    .join('');
+  
+  p.innerText = text;
+  
+  console.log(text);
 })
 
 recognition.start();
