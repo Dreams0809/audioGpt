45cba556f273b1da29ca2753d1af295cee3e203f
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2023-05-16 15:32:56 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -11,28 +11,6 @@ recognition.interimResults= true;
 
 let p = document.createElement('p');
 
-
-recognition.addEventListener('result',(e) => {
-  const text = Array.from(e.results)
-  .map(result => result[0])
-  .map(result => result.transcript)
-  .join("");
-
-  p.innerText = text
-  
-  texts.appendChild(p);
-  
-  console.log(text);
-  
-  
-
-})
-
-recognition.start();
-
-//Figure out a way to ask question, 
-//when i say a keyword moves on to next question
-
 //Adds an event listener to attach to a event with anew fucntion
 recognition.addEventListener('results',(e) => {
   
@@ -42,10 +20,63 @@ recognition.addEventListener('results',(e) => {
     .map(result => result[0])  
     .map(result => result.transcript)
     .join('');
-  
+    
+    //The inner text of the paragraph will be equal to text 
+ 
   p.innerText = text;
+  texts.appendChild(p);
+  
+  //If the event session results "isFinal = false" we need to make a new paragraph, which makes it true 
+  
+  if(e.results[0].isFinal){
+    if(text.includes('Hello') || text.includes('Hi')){
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'Hi, how was your day today';
+      texts.appendChild(p);
+    } 
+  }
+  
+  if(e.results[0].isfinal){
+    p = document.createElement('p');
+    p.classList.add('replay');
+    p.innerText = 'Did you learn anything new today?';
+    texts.appendChild(p);
+  }
+  
+  
+  if(e.results[0].isfinal){
+    p = document.createElement('p');
+    p.classList.add('replay');
+    p.innerText = 'What are your goals for the week ?';
+    texts.appendChild(p);
+  }
+  
+  
+  if(e.results[0].isfinal){
+    p = document.createElement('p')
+    p.classList.add('replay')
+    p.innerText = 'Did you read or workout today ?'
+    texts.appendChild(p);
+  }
+  
+  
+    if(e.results[0].isfinal){
+    p = document.createElement('p')
+    p.classList.add('replay')
+    p.innerText = 'How can you be a better version of yourself tomorrow? '
+    texts.appendChild(p);
+  }
+  
+   p = docuemnt.createElement('p');
+  
+  
   
   console.log(text);
+})
+
+recognition.addEventListener('end', ()=>{
+  recognition.start();
 })
 
 recognition.start();
