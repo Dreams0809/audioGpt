b1fd3b8f2e6b13c3ad9ba19ef635903eaa119268
Dreams0809 <expensivepain9@gmail.com>
2023-06-17 17:26:13 -0400

    nearly done

--- config/passport.js
+++ config/passport.js
@@ -112,6 +112,14 @@ module.exports = function (passport) {
     })
   );
 
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
 
   
 };
